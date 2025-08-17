abae88f0b1ad6b8bd496b60db34352835f270639
Keerthana <grittypuffy@riseup.net>
2025-08-17 12:41:56 +0530

    chore: add healthcheck for better reliability

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:20-alpine AS builder
+FROM node:22-alpine AS builder
 
 WORKDIR /app
 
@@ -19,7 +19,7 @@ ENV NEXT_PUBLIC_ENCRYPTION_IV=$NEXT_PUBLIC_ENCRYPTION_IV
 
 RUN npm run build
 
-FROM node:20-alpine
+FROM node:22-alpine AS runner
 
 WORKDIR /app
 
---
+++ pages/api/ping.ts
@@ -0,0 +1,19 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import connectToDatabase from "../../src/utils/db";
+import mongoose from "mongoose";
+
+export default async (
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> => {
+    try {
+        await connectToDatabase();
+        const result = await mongoose.connection.db.admin().ping();
+        if (result.ok) {
+            return res.status(200).json(result);
+        }
+        return res.status(500).json({ ok: 0 });
+    } catch (err) {
+        return res.status(500).json({ ok: 0 });
+    }
+};
